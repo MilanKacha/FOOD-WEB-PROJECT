29286c5a4a5caa92dbe8f4907c9e7780515736c0
Milan Kacha <kachamilan108@gmail.com>
2023-09-07 07:23:56 +0530

    add mongosanitize,xss,hpp

--- foodwebserver/index.js
+++ foodwebserver/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
-const { rateLimit } = require("express-rate-limit");
 const server = express();
+const rateLimit = require("express-rate-limit"); // 1 hr ma 100 req 1 id thi
+const helmet = require("helmet"); // helmet thi Http header security
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 const productRoute = require("./routes/productRoutes");
 const restorantRoute = require("./routes/restorantRoutes");
 const userRoute = require("./routes/userRoutes");
@@ -8,13 +12,24 @@ const userRoute = require("./routes/userRoutes");
 //1) middlewares
 server.use(express.json()); // to parse req.body
 
-// ratelimit middleware function
+server.use(helmet()); //Set Security Http headers
+
+// Limit req in 1 hour Same Id
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes time in miliseconds
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  message: "Too many requests from this IP, Please try again in an 15minutes!",
+  max: 100, // Limit each IP to 100 requests per `window`
+  windowMs: 60 * 60 * 1000, //60 minutes in milisec
+  message: "Too many requests from this IP, Please try again in an hour!",
 });
-server.use("/api", limiter); // it works on all route of /api
+// Apply the rate limiting middleware to all requests
+// Apply the rate limiting middleware to API calls only (/api)
+server.use("/api", limiter);
+
+//Data Sanitization against NoSQL query injection github ma 6e
+server.use(mongoSanitize());
+//Data Sanitization against XSS // Prevent against harm Html code
+server.use(xss());
+// prevant HTTP parameter pollution
+server.use(hpp());
 
 //3) Routes
 server.use("/api/v1/", productRoute);
